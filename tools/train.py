daac99412aa5830218b66143116a23dad7cb54aa
MeidiLprog <m.lef3105@gmail.com>
2025-12-04 00:12:22 +0100

    adding gridsearch for the agent to seek the best hyperparameters

--- tools/train.py
+++ tools/train.py
@@ -37,12 +37,28 @@ class TrainTool(Tool):
         X = df.drop(columns=[target_name],inplace=True)
         y = df[target_name]
 
-        train_X, test_X,train_y,test_y = train_test_split(X,y,random_state=42,test_size=0.2)
-
-        model = RandomForestClassifier()
+        X_train, X_test,y_train,y_test = train_test_split(X,y,random_state=42,test_size=0.2)
 
 
+        print("Let us figure out whether it's a classification or regression problem \n")
 
+        problem = "Classification" if y.nunique() <= 15 and y.dtype in [object,"int64","category"] else "regression"
+
+        if problem == "Classification":
+            model = RandomForestClassifier(random_state=42)
+            param_grid = {"n_estimators" : [10,35,40,100], 
+                          "max_depth" : [None,10,20,25,26,30,32,36]}
+
+        else:
+            model = RandomForestRegressor(random_state=42)
+            param_grid = {
+                "n_estimators" : [10,35,40,100],
+                "max_depth" : [None,10,20,25,26,30,32,36]
+            }
+
+        grid = GridSearchCV(model,param_grid,cv=3,n_jobs=-1)
+        grid.fit(X_train,y_train)
+        
 
         return "training done"        
 
