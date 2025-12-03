9272e001b0d420dc1b56fc04b9c006524c707372
MeidiLprog <m.lef3105@gmail.com>
2025-12-04 00:52:32 +0100

    starting to build the main

--- app.py
+++ app.py
@@ -2,3 +2,9 @@
 ## Lefki Meidi            ##
 ## First HuggingFace Agent##
 ############################
+
+import pandas as pd
+from agent_logic import create_agent
+
+url : str = "https://raw.githubusercontent.com/datasciencedojo/datasets/master/titanic.csv"
+df = pd.read_csv(url)
