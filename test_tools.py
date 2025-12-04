1ca226ef0046ca2824ad4dce1261c74819901a8e
MeidiLprog <m.lef3105@gmail.com>
2025-12-05 00:58:45 +0100

    final

--- test_tools.py
+++ test_tools.py
@@ -1,13 +1,16 @@
 import pandas as pd
 from tools.cleaning import CleaningTool
+from tools.inspect import InspectTool
+from tools.train import TrainTool
 
 
 
 df = pd.DataFrame({
-    "age" : [20,25,30,35,None,22],
-    "Ville": ["Paris","Nice",None,"Alger",None,None],
-    "Revenu": [None,None,500,None,600,522],
-    "constant" : [1,1,1,1,1,1]
+    "age" : [20,300,0,350,None,22,1560],
+    "Ville": ["Paris","Nice",None,"Alger",None,None,"Tlemcen"],
+    "Revenu": [None,None,500,None,600,522,1965],
+    "date": pd.date_range(start="2025-12-03 00:00:00", periods=7, freq="8H"),
+    "constant" : [1,1,1,1,1,1,1]
 
 })
 
@@ -15,5 +18,8 @@ print("Original Dataset\n")
 print(df)
 tool = CleaningTool()
 df_cleaned = tool.forward(df)
+ins = InspectTool()
+done_inspection = ins.forward(df)
+
 print("Dataset fully cleaned ! \n")
 print(df_cleaned)
