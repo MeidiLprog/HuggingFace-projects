369d63c976042c69fdcb62c4b01ab4094318c6b7
MeidiLprog <m.lef3105@gmail.com>
2025-12-04 13:30:46 +0100

    change of model, as qwen7:2b is way too demanding in ressources

--- agent_logic.py
+++ agent_logic.py
@@ -7,14 +7,13 @@ import smolagents
 from smolagents import CodeAgent, LiteLLMModel
 from tools.inspect import InspectTool
 from tools.cleaning import CleaningTool
-from tools.visu import PlotTool
-from tools.train import TrainingTool
+from tools.train import TrainTool    
 
 def create_agent():
-    model = LiteLLMModel(model_id="ollama/qwen2:7b")
+    model = LiteLLMModel(model_id="ollama/qwen2:1.5b")
     agent = CodeAgent(
         model=model,
-        tools=[InspectTool(),CleaningTool(),PlotTool(),TrainingTool()],
+        tools=[InspectTool(),CleaningTool(),TrainTool()],
         name="autodata_agent",
         description="HuggingFace agent created to carry out datascience/Ml tasks"
     )
