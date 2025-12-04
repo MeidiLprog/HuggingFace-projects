5720ddf45807674acc1384d82114fac53e21cf74
MeidiLprog <m.lef3105@gmail.com>
2025-12-04 01:06:50 +0100

    almost*

--- app.py
+++ app.py
@@ -8,3 +8,13 @@ from agent_logic import create_agent
 
 url : str = "https://raw.githubusercontent.com/datasciencedojo/datasets/master/titanic.csv"
 df = pd.read_csv(url)
+
+agent = create_agent()
+print("Agent ready !")
+
+agent.run(
+    "Inspect, clean and train model to predict survival on the dataset",
+    data=df,
+    target_name="Survived"
+
+)
